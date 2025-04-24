b7310497d8140748f8aa88150b18afee4a181fbc
Alex Heckman <aheckman1@mines.edu>
2025-04-24 01:47:30 -0600

    finished RLWE

--- RLWE/keyGen.py
+++ RLWE/keyGen.py
@@ -1,38 +1,68 @@
+from Polynomial_functions import polynomial_mod_mult, polynomial_mod_add, generate_random_poly_coef1, generate_random_poly, polynomial_mod_sub
 import numpy as np
-from random import SystemRandom
-import math
+from typing import Tuple
 
-def secret_key_gen(q,n):
-    cryptogen = SystemRandom() 
-    s =np.empty(n+1)
-    for i in range(0, n+1):
-        s[i] = cryptogen.randint(0, q)
-    s[0] = 1
-    return s
+def secret_key_gen(polynomial_degree : int   # the degree of the created polynomial
+                   ) -> np.array:
+    return generate_random_poly_coef1(polynomial_degree)
 
-def public_key_gen(q, n, N, s):
-    cryptogen = SystemRandom() 
+def generate_keys(polynomial_degree : int,   # the degree of the created polynomial
+                  ciphertext_modulus : int,   # the ciphertext modulus
+                  ) -> Tuple[np.array, np.array, np.array]:
+    # random key
+    s = secret_key_gen(polynomial_degree)
+    # random arr
+    a = generate_random_poly(polynomial_degree, ciphertext_modulus)
+    # random error
+    e = generate_random_poly_coef1(polynomial_degree)
+    # b = (-a*s) + e
+    b = polynomial_mod_add(- polynomial_mod_mult(a, s, ciphertext_modulus), e, ciphertext_modulus)
 
-    A = []
-    for i in range(0, n):
-        r = []
-        for j in range(0, N):
-            x = cryptogen.randint(0, q)
-            r.append(x)
-        A.append(r)
-    A = np.array(A)
+    return (a, b), s
 
-    e = np.empty(N)
-    for i in range(N):
-        e[i] = cryptogen.randint(0, q)
+#test code
+def encrypt2(m : str,                      # The message to be encrypted
+            pk : Tuple[np.array, np.array],     # The public key
+            ciphertext_modulus : int,           # The ciphertext modulus
+            plaintext_modulus : int             # The plaintext modulus
+            ) -> Tuple[np.array, np.array]:     # THe ciphertext
+    pk0, pk1 = pk
+    polynomial_degree = len(pk0)
+    u = generate_random_poly_coef1(polynomial_degree) 
+    e0 = generate_random_poly_coef1(polynomial_degree)
+    e1 = generate_random_poly_coef1(polynomial_degree)
 
-    b = np.dot(A, s[1:]) + 2 * e
-    b.resize(N, 1)
-    A = np.hstack((b, -1 * A)) % q
-    assert all(np.dot(A, s) % q == (2 * e) % q)
-    return A
+    #scale m up so that it isn't lost in the noise
+    m_scaled = np.array([((int(bit))*(ciphertext_modulus // plaintext_modulus)) % ciphertext_modulus for bit in m])
 
+    #c0 = pk0 * u + msg + e0
+    c0 = polynomial_mod_add(polynomial_mod_add(polynomial_mod_mult(u, pk1, ciphertext_modulus), m_scaled, ciphertext_modulus),e0, ciphertext_modulus)
+    #c1 = pk1 * u + e1
+    c1 = polynomial_mod_add(polynomial_mod_mult(u, pk0, ciphertext_modulus), e1, ciphertext_modulus)
+    
+    return c0, c1
+    
+def decrypt2(ciphertext: Tuple[np.array, np.array],         # Ciphertext
+              s: np.ndarray,                                # secret key
+              ciphertext_modulus: int,                      # The ciphertext modulus
+              plaintext_modulus: int) -> str:               # The plaintext modulus
+    
+    c0, c1 = ciphertext
+    #msg = c1 * s + c1
+    decrypted = polynomial_mod_add(c0, polynomial_mod_mult(c1, s, ciphertext_modulus), ciphertext_modulus)
+    #undo scaling
+    scaled = (decrypted * plaintext_modulus + ciphertext_modulus // 2) // ciphertext_modulus
+    #take the modulus
+    result = scaled % plaintext_modulus
+    ans = ""
+    for num in result:
+        ans += str(int(num))
+    return ans
 
-s = secret_key_gen(100, 5)
+public_key, secret_key = generate_keys(5, 1024)
+message = "11001"
+ciphertext = encrypt2(message, public_key, 1024, 2)
+decrypted_message = decrypt2(ciphertext, secret_key, 1024, 2)
 
-public_key_gen(100, 3, 4, s)
+print("Original Message:", message)
+print("Decrypted Message:", decrypted_message)
