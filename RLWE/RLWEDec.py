2d6b9b2e417c856c8c91f60690db7ad01ca99995
Alex Heckman <aheckman1@mines.edu>
2025-04-24 14:09:00 -0600

    commenting error

--- RLWE/RLWEDec.py
+++ RLWE/RLWEDec.py
@@ -27,7 +27,7 @@ def decrypt2(ciphertext: Tuple[np.array, np.array],         # Ciphertext
               plaintext_modulus: int) -> str:               # The plaintext modulus
     
     c0, c1 = ciphertext
-    #msg = c1 * s + c1
+    #msg = c1 * s + c0
     decrypted = polynomial_mod_add(c0, polynomial_mod_mult(c1, s, ciphertext_modulus), ciphertext_modulus)
     #undo scaling
     descaled = (decrypted * plaintext_modulus + ciphertext_modulus // 2) // ciphertext_modulus
